ee4e7fd170d7ee486d1e884b103fba796016bd9e
Lalit Thakre <lalit03thakre@gmail.com>
2022-12-30 17:42:54 +0530

    DSA

---
+++ Node.java
@@ -0,0 +1,15 @@
+// package com.linkedlist;
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
